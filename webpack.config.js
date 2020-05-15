e759bda9048e4ad3b3dc62ef480967554e7be328
VIJAY G <42581616+Vijay431@users.noreply.github.com>
2020-05-15 08:39:15 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './main.js',
+    output: { 
+        path: __dirname, 
+        filename: './scripts/bundle.js' 
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    "presets": ["@babel/preset-env", "@babel/preset-react"],
+                }
+            },
+            {
+                test : /\.css$/,
+                loader : 'style-loader'
+            },
+            {
+                test : /\.css$/,
+                loader : 'css-loader',
+                options : {
+                    sourceMap : true,
+                    importLoaders : 2,
+                    modules : {
+                        localIdentName : '[name]__[local]__[hash:base64:5]',
+                    }
+                }
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                loader: "file-loader?name=/Assets/images/[name].[ext]"
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+};
