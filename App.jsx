58bceab2caabd1d42bd6287af747ba1d7423da4b
VIJAY G <42581616+Vijay431@users.noreply.github.com>
2020-05-18 15:39:16 +0530

    updates

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 //Custom Pages .jsx
 import LandingPage from './Components/Others/landingPage.jsx';
@@ -10,18 +10,20 @@ import Shipping from './Components/Others/shipping.jsx';
 import ProductForm from './Components/Others/productForm.jsx';
 
 class App extends Component{
+   constructor(props){
+      super(props);
+   }
+
    render(){
       return(
          <React.Fragment>
             <Router>
-               <Switch>
                   <Route path="/" exact={true} component={LandingPage} />
-                  <Route path="/login" exact={true} component={Login} />
+                  <Route path="/login" component={Login} />
                   <Route path="/register" component={Register} />
                   <Route path="/cart" component={Cart} />
                   <Route path="/shipping" component={Shipping} />
                   <Route path="/productForm" component={ProductForm} />
-               </Switch>
             </Router>
          </React.Fragment>
       );
--- Components/Others/login.jsx
+++ Components/Others/login.jsx
@@ -4,7 +4,7 @@ import Styles from '../../Assets/css/login.css';
 
 class Login extends React.Component{ 
     constructor(props){
-        super(props);
+        super(props)
         this.state = {
             username: '',
             password: ''
@@ -16,31 +16,34 @@ class Login extends React.Component{
     }
 
     loginComponent(){
-        console.log("Login");
     }
 
     render(){
         return(
-            <div>
+            <div className={Styles.Card} >
                 <div className="card" >
                     <h3 className="card-header" >Login</h3>
                     <div className="card-body" >
                         <div className="card-text" >
-                            <input type="text" name="username" value={this.username} onChange={() => this.formValidator(event)} />
+                            <label htmlFor="username" >Username</label> : 
+                            <input type="text" name="username" id="username" value={this.username} onChange={() => this.formValidator(event)} />
                         </div>
                         <div className="card-text">
-                            <input type="password" name="password" value={this.password} onChange={() => this.formValidator(event)} />
+                            <label htmlFor="password" >Password</label> : 
+                            <input type="password" name="password" id="password" value={this.password} onChange={() => this.formValidator(event)} />
                         </div>
-                        <div className={Styles.ButtonGroup} >
+                        <div className="btn btn-group" >
                             <div className={Styles.Button} >
-                                <button type="submit" onClick={this.loginComponent} >Login</button>
+                                <button type="submit" className="btn btn-success" onClick={this.loginComponent} >Login</button>
                             </div>
                             <div className={Styles.Button} >
-                                <a href="/" ><button type="button" >Cancel</button></a>
+                                <a href="/" ><button type="button" className="btn btn-danger" >Cancel</button></a>
                             </div>
                         </div>
-                        <div className="card-footer" >
-                            <a href="/register" >Are you a new visitor?.Click here!</a>
+                    </div>
+                    <div className="card-footer" >
+                        <div className={Styles.Footer} >
+                            <a id="hreflink" href="/register" >Are you a new visitor?.click here!</a>
                         </div>
                     </div>
                 </div>
